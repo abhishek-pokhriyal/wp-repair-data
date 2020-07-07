cc6ea5939842c14bd1344b804125d287d4a14f68
abhishek-pokhriyal <pokhi@coloredcow.com>
2020-07-07 13:12:54 +0530

    Adjusting order total after renewal is created

--- includes/class-wp-repair-data.php
+++ includes/class-wp-repair-data.php
@@ -19,7 +19,7 @@ final class WP_Repair_Data {
 	 *
 	 * @var string
 	 */
-	public $version = '0.0.11';
+	public $version = '1.0.0';
 
 	/**
 	 * The single instance of the class.
--- includes/class-wprd-refund-missing-discount.php
+++ includes/class-wprd-refund-missing-discount.php
@@ -2,7 +2,7 @@
 /**
  * WP Renewal Discount.
  *
- * @since   0.0.12
+ * @since   1.0.0
  */
 
 defined( 'ABSPATH' ) || exit;
@@ -17,12 +17,34 @@ class WPRD_Refund_Missing_Discount {
 			return;
 		}
 
-		// Add actions/filters to subtract the discount amount from the order.
+		add_filter( 'wcs_renewal_order_created', __CLASS__ . '::adjust_missing_discount', 99, 2 );
 	}
 
+	public static function adjust_missing_discount( $renewal_order, $subscription ) {
+		$missing_discount = self::get_missing_discount_amount( $subscription->get_id() );
+		$original_total   = $renewal_order->get_total();
+
+		/**
+		 * @todo: Confirm that the $original_total contains the 5% discount.
+		 */
+		$adjusted_total   = $original_total - $missing_discount;
+
+		$renewal_order->set_total( $adjusted_total );
+		$renewal_order->save();
+	}
+
+	private static function get_missing_discount_amount( $order_id ) {
+		$order_discounts = require_once WPRD_ABSPATH . 'data/missing-discounts.php';
+
+		if ( array_key_exists( $order_id, $order_discounts ) ) {
+			return floatval( $order_discounts[ $order_id ] );
+		}
+
+		return 0;
+	}
 
 	/**
-	 * Check whether an order needs to refunded.
+	 * Check whether an order needs a refund amount.
 	 *
 	 * @param  int $order_id
 	 * @return bool
